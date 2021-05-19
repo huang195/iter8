a88c712cb658c0387ca1f784ee1eb71319ca035a
Hai Huang <huang195@gmail.com>
2021-05-19 11:39:17 -0400

    reorg dir

--- samples/gitops/runCI.sh
+++ samples/gitops/runCI.sh
@@ -3,3 +3,5 @@
 URL_VALUE="http://$(kubectl -n istio-system get svc istio-ingressgateway -o jsonpath='{.spec.clusterIP}'):80/productpage"
 sed "s|value: URL_VALUE|value: $URL_VALUE|" upgrade/fortio.yaml > ./fortio.yaml
 cp upgrade/productpage-candidate.yaml ./productpage-candidate.yaml
+RANDOM=`od -An -N4 -i /dev/random`
+SED "s|name: gitops-exp|name: gitops-exp-$RANDOM|" upgrade/experiment.yaml > ./experiment.yaml
