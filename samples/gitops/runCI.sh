60eb5e66aeade42649a53da0bccf568d9d4e956e
Hai Huang <huang195@gmail.com>
2021-05-20 12:04:49 -0400

    shorten experiment

--- samples/gitops/runCI.sh
+++ samples/gitops/runCI.sh
@@ -4,4 +4,4 @@ URL_VALUE="http://$(kubectl -n istio-system get svc istio-ingressgateway -o json
 sed "s|value: URL_VALUE|value: $URL_VALUE|" upgrade/fortio.yaml > ./fortio.yaml
 cp upgrade/productpage-candidate.yaml ./productpage-candidate.yaml
 RANDOM=`od -An -N4 -i /dev/random`
-SED "s|name: gitops-exp|name: gitops-exp-$RANDOM|" upgrade/experiment.yaml > ./experiment.yaml
+sed "s|name: gitops-exp|name: gitops-exp-$RANDOM|" upgrade/experiment.yaml > ./experiment.yaml
