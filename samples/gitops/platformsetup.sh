7f3027a68afa18e280f0ddbd89624ab4ee60f749
Hai Huang <huang195@gmail.com>
2021-05-24 11:14:59 -0400

    clean up

--- samples/gitops/platformsetup.sh
+++ samples/gitops/platformsetup.sh
@@ -91,6 +91,6 @@ echo "Verifying Argo CD installation"
 kubectl wait --for condition=ready --timeout=300s pods --all -n argocd
 echo "Your Argo CD installation is complete"
 echo "Run the following commands: "
-echo "  1. kubectl port-forward svc/argocd-server -n argocd 8080:443'"
+echo "  1. kubectl port-forward svc/argocd-server -n argocd 8080:443"
 echo "  2. Open a browser with URL: http://localhost:8080 with the following credential"
 echo "     Username: 'admin', Password: `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`"
