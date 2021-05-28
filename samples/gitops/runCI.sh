52a932bd8f858f4560f5d8a41968fbe1ca0edc15
Hai Huang <huang195@gmail.com>
2021-05-28 09:48:55 -0400

    revert argocd changes

--- samples/gitops/runCI.sh
+++ samples/gitops/runCI.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-# use Fortio as a workload generator - use a random name so workload generator restarts on new experiment
-cp templates/fortio.yaml ./fortio.yaml
-
-# start a experiment targetting a fixed name baseline and candidate
-cp templates/experiment.yaml ./experiment.yaml
+# give fortio deployment a random name so it restarts it upon a new experiment
+RANDOM=`od -An -N4 -i /dev/random`
+sed "s|  name: fortio-|  name: fortio-$RANDOM|" templates/fortio.yaml > ./fortio.yaml
 
 # use a random color for a new experiment candidate
 declare -a colors=("red" "orange" "blue" "green" "yellow" "violet" "brown")
 color=`expr $RANDOM % ${#colors[@]}`
-version=`git rev-parse HEAD`
+version=`cat templates/version`
+version=`expr $version + 1`
 sed "s|value: COLOR|value: \"${colors[$color]}\"|" templates/productpage-candidate.yaml |\
 sed "s|version: v.*|version: v$version|" > ./productpage-candidate.yaml
+echo $version > templates/version
 
+# give experiment a random name so CI triggers new experiment each time a new app version is available
+sed "s|name: gitops-exp|name: gitops-exp-$RANDOM|" templates/experiment.yaml > ./experiment.yaml
