53acddda08141299e3e6758ac1307f8ffccd9226
Hai Huang <huang195@gmail.com>
2021-06-09 08:51:38 -0400

    add app

--- samples/cicd/app/server.py
+++ samples/cicd/app/server.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 import http.server
 import socketserver
 from http import HTTPStatus
+
+version = os.getenv('version')
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world v1\n')
+        resp = 'Hello world! (version: %s)\n' % (version)
+        self.wfile.write(str.encode(resp))
 
 httpd = socketserver.TCPServer(('0.0.0.0', 8000), Handler)
 httpd.serve_forever()
